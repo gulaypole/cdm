3c6a594f5450965bc7e547918a735337dc4819f3
dttric <87319664+dttric@users.noreply.github.com>
2024-07-18 23:33:05 +0700

    first commit

---
+++ src/main/java/dkn/minecraft/CDM.kt
@@ -0,0 +1,13 @@
+package dkn.minecraft
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CDM : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
